dddb0edddbc3e8c21a788c3d91f1636b79381a2f
RaulC2803 <raul_casanova_28@hotmail.com>
2021-05-20 18:46:57 -0500

    http GET of get carwashes from user's liked list

--- src/main/java/com/ezwash/backend/domain/service/accounts/UserService.java
+++ src/main/java/com/ezwash/backend/domain/service/accounts/UserService.java
@@ -3,6 +3,8 @@ package com.ezwash.backend.domain.service.accounts;
 import com.ezwash.backend.domain.model.accounts.CarWash;
 import com.ezwash.backend.domain.model.accounts.User;
 import com.ezwash.backend.domain.model.geographic.Location;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,4 +12,5 @@ public interface UserService {
     User createUser(User user, Location location);
     User findUserById(Long id);
     User addUserCarwash(Long userId, Long carWashId);
+    Page<CarWash> getLikedList(Long userId, Pageable pageable);
 }
