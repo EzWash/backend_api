a02052194cdc5facdd44b5ab829145bbdfe7b3cd
Drexlarr <diegodba29@gmail.com>
2021-06-16 22:06:06 -0500

    feat: designateStaffToContract has been done

--- src/main/java/com/ezwash/backend/service/business/ContractServiceImpl.java
+++ src/main/java/com/ezwash/backend/service/business/ContractServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ezwash.backend.service.business;
 
 import com.ezwash.backend.domain.model.accounts.Customer;
+import com.ezwash.backend.domain.model.accounts.Staff;
 import com.ezwash.backend.domain.model.business.Contract;
 import com.ezwash.backend.domain.model.business.Service;
 import com.ezwash.backend.domain.repository.accounts.CustomerRepository;
+import com.ezwash.backend.domain.repository.accounts.StaffRepository;
 import com.ezwash.backend.domain.repository.business.ContractRepository;
 import com.ezwash.backend.domain.repository.business.ServiceRepository;
 import com.ezwash.backend.domain.service.business.ContractService;
@@ -16,6 +18,9 @@ public class ContractServiceImpl implements ContractService {
     private CustomerRepository customerRepository;
 
     @Autowired
+    private StaffRepository staffRepository;
+
+    @Autowired
     private ContractRepository contractRepository;
 
     @Autowired
@@ -26,5 +31,18 @@ public class ContractServiceImpl implements ContractService {
                 .orElseThrow(() -> new ResourceNotFoundException("Contract", "Id", id));
     }
 
+    @Override
+    public Contract designateStaffToContract(Long contractId, Long staffId){
+
+        Staff staff = staffRepository.findById(staffId)
+                .orElseThrow(() -> new ResourceNotFoundException("Staff", "Id", staffId));
+        Contract contract = contractRepository.findById(contractId)
+                .orElseThrow(() -> new ResourceNotFoundException("Contract", "Id", contractId));
+
+        contract.setStaff(staff);
+
+        return contractRepository.save(contract);
+
+    }
 
 }
